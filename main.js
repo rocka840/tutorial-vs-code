e1dd9bbf65e815ea70387c49d58e2634c657a52f
rocka840 <rocka840@school.lu>
2019-09-23 11:12:57 +0200

    number var

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// alerts
 a = 20000;
 b = 40000;
 c = a + b;
-
 alert("This is an alert");
+
+// variables - strings
+var name = "Katherine";
+alert(name);
+
+// var numbers
+var originalNum = 45;
+var numToBeAdded = 5;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum);
