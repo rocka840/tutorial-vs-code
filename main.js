365b78d29e52fa3d4204a495b403fc72ead57b2f
rocka840 <rocka840@school.lu>
2019-09-23 11:46:22 +0200

    kkk

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-// alerts
+//1- alerts
 a = 20000;
 b = 40000;
 c = a + b;
 alert("This is an alert");
 
-// variables - strings
+//2- variables - strings
 var name = "Katherine";
 alert(name);
 
-// var numbers
+//3-  var numbers
 var originalNum = 45;
 var numToBeAdded = 5;
 var newNum = originalNum + numToBeAdded;
 console.log(newNum);
+
+//6- Math
+var num = 1;
+var NENumm = num++;
+console.log(num, NENumm);
+
+// if u put ++ infront, its different
+var num = 1;
+var NewNewNum = ++num;
+console.log(num, NewNewNum);
